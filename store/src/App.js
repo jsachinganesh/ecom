0595f60e0ac07f59da2ff062d868644db081baa1
jsachinganesh <jsachinganesh@gmail.com>
2020-11-07 12:39:47 +0530

    shops

--- store/src/App.js
+++ store/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/homepage/HomePage';
+import ShopPage from './pages/shop/Shop';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
---
+++ store/src/pages/shop/Shop.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import PreviewCollection from '../../components/preview-collection/Preview-Collection';
+import SHOP_DATA from './shop.data'
+
+class Shop extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collections:SHOP_DATA
+        }
+    }
+    render() {
+        const { collections } = this.state;
+        return (
+            <div className="shop-page">
+                {collections.map(({id,...otherCollectionProps}) => (
+                    <PreviewCollection key={id} {...otherCollectionProps}/>
+               ))}
+            </div>
+        );
+    }
+}
+
+export default Shop;
